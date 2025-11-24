820b3ce6f91731e52bc2e6f83753667496611fa1
vintg <10236284+vintg@users.noreply.github.com>
2025-11-24 02:13:26 -0500

    refactor(backend): switch to nvidia llama due to api key issue with openai via nvidia

--- backend/app/llm_client.py
+++ backend/app/llm_client.py
@@ -1,7 +1,17 @@
 import os
-from openai import OpenAI
+# from openai import OpenAI
+#
+# client = OpenAI(
+#     api_key=os.getenv("NVIDIA_API_KEY"),
+#     base_url="https://integrate.api.nvidia.com/v1"
+# )
 
-client = OpenAI(
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
+client = ChatNVIDIA(
+    model="meta/llama-3.2-3b-instruct",
     api_key=os.getenv("NVIDIA_API_KEY"),
-    base_url="https://integrate.api.nvidia.com/v1"
+    temperature=0.2,
+    top_p=0.7,
+    max_tokens=1024,
 )
